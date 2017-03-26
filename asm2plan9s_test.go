76404be549a378b930ad9b2941485f4d36124e4e
Chad Kunde <Kunde21@gmail.com>
2017-03-26 14:55:26 -0700

    Replaced slice of strings operations with byte slice operations
    Preparing to remove beautification from yasm function.

--- asm2plan9s_test.go
+++ asm2plan9s_test.go
@@ -1,69 +1,70 @@
 package main
 
-import (
-	"testing"
-)
+import "testing"
 
 func TestInstruction(t *testing.T) {
 
-	ins := "                                 // VPADDQ  XMM0,XMM1,XMM8"
-	out := "    LONG $0xd471c1c4; BYTE $0xc0 // VPADDQ  XMM0,XMM1,XMM8"
+	ins := "                                 // + VPADDQ  XMM0,XMM1,XMM8"
+	out := "    LONG $0xd471c1c4; BYTE $0xc0 // + VPADDQ  XMM0,XMM1,XMM8"
 
-	result, _ := assemble([]string{ins})
+	result, err := assemble([]byte(ins))
+	if err != nil {
+		t.Error(err)
+	}
 
-	if result[0] != out {
-		t.Errorf("expected %s\ngot                     %s", out, result[0])
+	if string(result) != out {
+		t.Errorf("expected %s\ngot                     %s", out, string(result))
 	}
 
 }
 
 func TestInstructionPresent(t *testing.T) {
 
-	ins := "    LONG $0xd471c1c4; BYTE $0xc0 // VPADDQ  XMM0,XMM1,XMM8"
-	out := "    LONG $0xd471c1c4; BYTE $0xc0 // VPADDQ  XMM0,XMM1,XMM8"
+	ins := "    LONG $0xd471c1c4; BYTE $0xc0 // + VPADDQ  XMM0,XMM1,XMM8"
+	out := "    LONG $0xd471c1c4; BYTE $0xc0 // + VPADDQ  XMM0,XMM1,XMM8"
 
-	result, _ := assemble([]string{ins})
+	result, _ := assemble([]byte(ins))
 
-	if result[0] != out {
-		t.Errorf("expected %s\ngot                     %s", out, result[0])
+	if string(result) != out {
+		t.Errorf("expected %s\ngot                     %s", out, string(result))
 	}
 
 }
 
 func TestInstructionWrongBytes(t *testing.T) {
 
-	ins := "    LONG $0x003377bb; BYTE $0xff // VPADDQ  XMM0,XMM1,XMM8"
-	out := "    LONG $0xd471c1c4; BYTE $0xc0 // VPADDQ  XMM0,XMM1,XMM8"
+	ins := "    LONG $0x003377bb; BYTE $0xff // + VPADDQ  XMM0,XMM1,XMM8"
+	out := "    LONG $0xd471c1c4; BYTE $0xc0 // + VPADDQ  XMM0,XMM1,XMM8"
 
-	result, _ := assemble([]string{ins})
+	result, _ := assemble([]byte(ins))
 
-	if result[0] != out {
-		t.Errorf("expected %s\ngot                     %s", out, result[0])
+	if string(result) != out {
+		t.Errorf("expected %s\ngot                     %s", out, string(result))
 	}
 
 }
 
 func TestInstructionInDefine(t *testing.T) {
 
-	ins := `    LONG $0x00000000; BYTE $0xdd                               \ // VPADDQ  XMM0,XMM1,XMM8`
-	out := `    LONG $0xd471c1c4; BYTE $0xc0                               \ // VPADDQ  XMM0,XMM1,XMM8`
+	ins := `    LONG $0x00000000; BYTE $0xdd                               \ // + VPADDQ  XMM0,XMM1,XMM8`
+	out := `    LONG $0xd471c1c4; BYTE $0xc0 \ // + VPADDQ  XMM0,XMM1,XMM8`
 
-	result, _ := assemble([]string{ins})
+	result, _ := assemble([]byte(ins))
 
-	if result[0] != out {
-		t.Errorf("expected %s\ngot                     %s", out, result[0])
+	if string(result) != out {
+		t.Errorf("expected %s\ngot                     %s", out, string(result))
 	}
 
 }
 
 func TestLongInstruction(t *testing.T) {
 
-	ins := "                                   // VPALIGNR XMM8, XMM12, XMM12, 0x8"
-	out := "    LONG $0x0f1943c4; WORD $0x08c4 // VPALIGNR XMM8, XMM12, XMM12, 0x8"
+	ins := "                                   // + VPALIGNR XMM8, XMM12, XMM12, 0x8"
+	out := "    LONG $0x0f1943c4; WORD $0x08c4 // + VPALIGNR XMM8, XMM12, XMM12, 0x8"
 
-	result, _ := assemble([]string{ins})
+	result, _ := assemble([]byte(ins))
 
-	if result[0] != out {
-		t.Errorf("expected %s\ngot                     %s", out, result[0])
+	if string(result) != out {
+		t.Errorf("expected %s\ngot                     %s", out, string(result))
 	}
 }
